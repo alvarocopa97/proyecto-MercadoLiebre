914ff9f57a82f31a7ee6fb3957ee3f19ae210266
alvarocopa <alvaro-copa2011@hotmail.com>
2023-08-10 21:15:25 -0300

    modificando el package json para el render

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3002
 app.use(express.static('public'));
 
 
-app.listen(3002, ()=>{
-    console.log('Servidor funcionando en el purto 3001');
-});
+app.listen(port, ()=> console.log(`Servidor corriendo en el purto ${port}`));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
